d375774469b7a965464adbc416e3b145ec2cdc1b
Aryan Agrawal <80872992+Codes-of-Aryan@users.noreply.github.com>
2022-03-14 18:52:54 +0530

    We have the word
    Word is now being displayed on the webpage on index.html

---
+++ message.js
@@ -0,0 +1,14 @@
+/*function msg() {
+    alert("Hello Javatpoint");
+}*/
+
+//var index = 10;
+//document.getElementById("index").innerHTML = index;
+
+var simple = function () {
+    var textMultiple = {
+        text1: "bullsheet",
+        text2: "text2"
+    };
+    return textMultiple;
+}
